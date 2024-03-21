16ebf794cd776d81a7cd4bf70a45cb8dd2a06dc1
A. D. R. Lakshani <22644135+srkrose@users.noreply.github.com>
2024-03-21 11:53:56 +0530

    Update lsphpstatus.sh

--- service/lsphpstatus.sh
+++ service/lsphpstatus.sh
@@ -128,7 +128,7 @@ function send_data() {
 function send_sms() {
 	message=$(echo "$hostname: $content")
 
-	php $scripts/send_sms.php "$message" "$validation"
+	#php $scripts/send_sms.php "$message" "$validation"
 
 	curl -X POST -H "Content-type: application/json" --data "{\"text\":\"$message\"}" $serviceslack
 }
